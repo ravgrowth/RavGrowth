906bcdee78527b8d999e50b2b853aebefe8a86d9
Tyler Ali <contact@ravgrowth.com>
2025-06-22 14:33:48 -0400

    Add send-email endpoint

--- api/send-email.js
+++ api/send-email.js
@@ -1,7 +1,7 @@
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses'
 
 const ses = new SESClient({
-  region: 'us-east-1',   // Change if needed!
+  region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
