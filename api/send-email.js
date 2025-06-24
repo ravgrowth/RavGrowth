3cf472413d8f8fe015f000cac9a7350ab1e85fe8
Tyler Ali <contact@ravgrowth.com>
2025-06-24 18:34:26 -0400

    aws not working so i fix

--- api/send-email.js
+++ api/send-email.js
@@ -3,8 +3,8 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 const ses = new SESClient({
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
