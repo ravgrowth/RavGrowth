d0026f96152dafa2f9aa9566c3da7162706eda82
Tyler Ali <contact@ravgrowth.com>
2025-06-26 16:23:01 -0400

    Fix supabase import in Admin.jsx (2x)

--- src/SimpleLogin.jsx
+++ src/SimpleLogin.jsx
@@ -1,6 +1,6 @@
 // SimpleLogin.jsx
 import { useState } from 'react'
-import supabase from './supabaseClient'
+import { supabase } from './supabaseClient'
 
 export default function SimpleLogin() {
   const [email, setEmail] = useState('')
