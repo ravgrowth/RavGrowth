e7e0bd34ef7bfdc455a0446404837162db00875a
Tyler Ali <contact@ravgrowth.com>
2025-06-26 16:21:41 -0400

    Fix supabase import in Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import supabase from './supabaseClient'
+import { supabase } from './supabaseClient'
 
 export default function Admin() {
   const [allowed, setAllowed] = useState(false)
